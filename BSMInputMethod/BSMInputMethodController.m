79af1cda98e271eb986fd63110396cb2da356093
Francis Chong <francis@ignition.hk>
2013-05-11 01:36:35 +0800

    minor refactor

--- BSMInputMethod/BSMInputMethodController.m
+++ BSMInputMethod/BSMInputMethodController.m
@@ -21,11 +21,11 @@
 
 -(BOOL)inputText:(NSString*)string key:(NSInteger)keyCode modifiers:(NSUInteger)flags client:(id)sender {
     if (keyCode >= kVK_ANSI_Keypad0 && keyCode <= kVK_ANSI_Keypad9) {
-        [self appendBuffer:string client:sender];
-        return YES;
+        return [self appendBuffer:string client:sender];
+
     } else if (keyCode == kVK_ANSI_KeypadMinus) {
-        [self minusLastBuffer:sender];
-        return YES;
+        return [self minusLastBuffer:sender];
+
     } else if (keyCode == kVK_ANSI_KeypadDecimal) {
 
     } else if (keyCode == kVK_ANSI_KeypadEnter) {
@@ -40,11 +40,6 @@
 }
 
 -(BOOL) appendBuffer:(NSString*)string client:(id)sender {
-    // cannot accept more input if input is 6
-    if ([_inputBuffer length] >= 6) {
-        return NO;
-    }
-
     [_inputBuffer appendString:string];
     _insertionIndex++;
 
@@ -55,14 +50,17 @@
     return YES;
 }
 
-- (void)minusLastBuffer:(id)sender {
+- (BOOL) minusLastBuffer:(id)sender {
 	if ( _insertionIndex > 0 && _insertionIndex <= [_inputBuffer length] ) {
 		--_insertionIndex;
         [_inputBuffer deleteCharactersInRange:NSMakeRange(_insertionIndex,1)];
         [sender setMarkedText:_inputBuffer
                selectionRange:NSMakeRange(_insertionIndex, 0)
              replacementRange:NSMakeRange(NSNotFound,NSNotFound)];
-	}
+        return YES;
+	} else {
+        return NO;
+    }
 }
 
 @end
